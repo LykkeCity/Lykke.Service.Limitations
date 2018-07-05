cb15fb0deae67fd3c2889d1f0778fc28289a4dca
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-07-05 16:45:04 +0300

    Removed unused code for withdrawals

--- src/Lykke.Service.Limitations.Core/Domain/IWithdrawLimit.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lykke.Service.Limitations.Core.Domain
-{
-    public interface IWithdrawLimit
-    {
-        string AssetId { get; }
-        double LimitAmount { get; }
-    }
-}
--- src/Lykke.Service.Limitations.Core/Domain/WithdrawLimit.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lykke.Service.Limitations.Core.Domain
-{
-    public class WithdrawLimit : IWithdrawLimit
-    {
-        public string AssetId { get; set; }
-
-        public double LimitAmount { get; set; }
-    }
-}
