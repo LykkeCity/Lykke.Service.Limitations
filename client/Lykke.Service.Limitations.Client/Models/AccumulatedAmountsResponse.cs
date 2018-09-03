309417f7532c7818f748baa13fa5cdef3363ba21
igorag <igorag@mmtr.ru>
2018-09-03 18:16:33 +0300

    LWDEV-7748-calculate-amount

---
+++ client/Lykke.Service.Limitations.Client/Models/AccumulatedAmountsResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.Service.Limitations.Client.Models
+{
+    public class AccumulatedAmountsResponse
+    {
+        public double DepositTotalFiat { get; set; }
+
+        public double Deposit30DaysFiat { get; set; }
+
+        public double Deposit1DayFiat { get; set; }
+
+        public double DepositTotalNonFiat { get; set; }
+
+        public double Deposit30DaysNonFiat { get; set; }
+
+        public double Deposit1DayNonFiat { get; set; }
+
+        public double WithdrawalTotalFiat { get; set; }
+
+        public double Withdrawal30DaysFiat { get; set; }
+
+        public double Withdrawal1DayFiat { get; set; }
+
+        public double WithdrawalTotalNonFiat { get; set; }
+
+        public double Withdrawal30DaysNonFiat { get; set; }
+
+        public double Withdrawal1DayNonFiat { get; set; }
+    }
+}
